0400fa48d93f7ed400889fa632c150cc05b6b19e
Sivanandhinikannan <sweetysiva07@gmail.com>
2022-02-10 08:52:34 +0530

    Codes Added

---
+++ src/org/git/Sample.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Sample {
+
+	public void g1() {
+		System.out.println("Welcome to git");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi.....");
+	}
+}
