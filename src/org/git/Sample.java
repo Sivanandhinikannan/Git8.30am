97af92f04c288de24e6aa703f1c92ca7a6619466
Sivanandhinikannan <sweetysiva07@gmail.com>
2022-02-10 09:20:10 +0530

    Chandler done coding

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -6,9 +6,15 @@ public class Sample {
 		System.out.println("Welcome to git");
 	}
 	
+
+	public void g2() {
+		System.out.println("Welcome to git---2");
+	}
+	
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hi.....");
+		System.out.println("Coding done by chandler");
 	}
 }
