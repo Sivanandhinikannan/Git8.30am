17068b495f4a8b9f6e094ac0229c7c1be8cc5de7
Sivanandhinikannan <sweetysiva07@gmail.com>
2022-02-10 09:29:09 +0530

    Phoebe coding...

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -6,9 +6,14 @@ public class Sample {
 		System.out.println("Welcome to git");
 	}
 	
+	public void g3() {
+		System.out.println("Phoebe....");
+	}
+	
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hi.....");
+		System.out.println("Phoebe done coding.....");
 	}
 }
